89f5c0c76bf8726e12e7845a6e4ce6a80fedaac7
smrafi <smrafi4130@gmail.com>
2018-12-20 22:58:37 +0600

    first time

---
+++ js/firebase.js
@@ -0,0 +1,47 @@
+// Reference messages collection
+var messagesRef = new Firebase('https://portfolio-8e44e.firebaseio.com/portfolio');
+//var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, email, message);
+
+    // // Show alert
+    // document.querySelector('.alert').style.display = 'block';
+
+    // // Hide alert after 3 seconds
+    // setTimeout(function(){
+    //   document.querySelector('.alert').style.display = 'none';
+    // },3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+
+        email: email,
+
+        message: message
+    });
+}
